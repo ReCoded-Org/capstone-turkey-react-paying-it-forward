9bf030bc715314658e560aaf4a38d6d4163059bc
androidmini9x <androidmini92@gmail.com>
2022-04-25 19:17:48 +0300

    Fix: fix some bugs && re-structure the files

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import './App.css';
 // import Faq from './pages/FAQ/Faq';
 // import ContactUs from './pages/ContactUs/ContactUs';
 import Navbar from './components/Navbar/Navbar';
-import Request from './pages/Request/Request';
+// import Request from './pages/Request/Request';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar isLogin />
-        <Request />
       </BrowserRouter>
     </div>
   );
