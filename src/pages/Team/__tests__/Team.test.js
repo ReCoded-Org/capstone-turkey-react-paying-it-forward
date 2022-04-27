0ab9a66aa486d932649a64e77087d0572c409a0d
androidmini9x <androidmini92@gmail.com>
2022-04-27 19:16:37 +0300

    Fix: fix node cli errors

--- src/pages/Team/__tests__/Team.test.js
+++ src/pages/Team/__tests__/Team.test.js
@@ -1,16 +1,19 @@
-import { render, screen, fireEvent, wait } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Team from '../Team';
 
-test('Test Team Page', () => {
+test('Team Page', () => {
+  const members = [
+    'Kutay Kağan Özen',
+    'Khadija Hawa',
+    'Şebnem Görmüş',
+    'Abuobaida Abdi',
+    'Göksu Alkan',
+    'Mustafa Durmaz',
+  ];
 
-    const members = [
-        'Kutay Kağan Özen', 'Khadija Hawa', 'Şebnem Görmüş', 'Abuobaida Abdi', 'Göksu Alkan', 'Mustafa Durmaz'
-    ];
+  render(<Team />);
 
-    render(<Team />);
-
-    for (let i = 0; i < members.length; i++) {
-        expect(screen.getByText(members[i])).toBeInTheDocument();
-    }
-
+  for (let i = 0; i < members.length; i++) {
+    expect(screen.getByText(members[i])).toBeInTheDocument();
+  }
 });
