543da6670b2acb637d24e098649a79a408950d6e
androidmini9x <androidmini92@gmail.com>
2022-05-04 19:39:33 +0300

    Fix: merge conflic

--- src/components/Login/tests/Login.test.js
+++ src/components/Login/tests/Login.test.js
@@ -1,14 +1,19 @@
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import Login from '../Login';
+import store from '../../../store';
 
 describe('Login Component', () => {
   it('matches', () => {
     const tree = renderer
       .create(
-        <Router>
-          <Login />
-        </Router>,
+        <Provider store={store}>
+          <Router>
+            <Login />
+          </Router>
+        </Provider>,
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
